7769b774c8a9dc16cb25f10a7ecccf8f61946929
Izuchukwu Ijeudo <izuchukwuijeudo@gmail.com>
2024-05-31 07:00:31 +0100

    mail gun

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -2,9 +2,11 @@ package application;
 
 import application.config.ProjectConfig;
 import application.services.HelloService;
+import application.services.MailService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 
@@ -15,5 +17,11 @@ public class Main {
         System.out.println("beans in context: " + Arrays.toString(context.getBeanDefinitionNames()));
         HelloService helloService = context.getBean(HelloService.class);
         helloService.sayHello("Doe");
+
+        BigDecimal amount = context.getBean(BigDecimal.class);
+        System.out.println(amount);
+
+        MailService mailService = context.getBean(MailService.class);
+        mailService.sendMailto("operations@gmail.com");
     }
 }
